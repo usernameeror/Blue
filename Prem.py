6206d4dfbb3135a6c70ebe12b60d30dec03aee10
BINTANG XD <94116284+usernameeror@users.noreply.github.com>
2021-11-12 12:17:22 +0700

    Update Prem.py

--- Prem.py
+++ Prem.py
@@ -1,12 +1,12 @@
 ##From LIB lmprot famz
 ##blue=['\033[34;1m'LAM-TIM
-##by BINTANG XD
-##[1] Login via token
-##[2] login cokies
-##1FROM FRADLIS/TEMAN
-##2FROM ID MASAL
-##3FROM ID GRUP
-##4FROM ID OLD
-##5CEK HASIL CRACK
-##6USER AGENT
-##00HAPUS TOKEN/COKIES
+##green=\'033[32;1m'https://github.com/usernameeror/Blue/edit/main/Prem.py
+##green=\'033[32;1m'IP:
+##yellow=\'033['33;1m'[01]Login via Token
+##yellow=\'033['33;1m'[02]Login via Cokies
+##yellow=\'033['33;1m'[01]FROM ID PUBLIK
+##yellow=\'033['33;1m'[02]FROM ID GRUP
+##yellow=\'033['33;1m'[03]FROM ID OLD
+##yellow=\'033['33;1m'[04]CEK HASIL CRACK
+##yellow=\'033['33;1m'[05]CEK USER AGENT
+##yellow=\'033['33;1m'[00]HAPUS TOKEN/COKIES
